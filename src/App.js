80f9044c0f83bd508c38175dc255525bd80cd1a2
Erdij <yildirimsukruerdi@gmail.com>
2022-11-17 17:40:40 +0300

    fakeApi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState("");
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setData(res.data[id].title));
+  }, [data, id]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
+      <p>
+        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      </p>
     </div>
   );
 }
-
 export default App;
